8ad3e862b1bac0190384ccea464acda027e3e07d
natadana.05 <natadana.05@gmail.com>
2025-09-22 16:48:35 +0000

    pertemuan-4

--- lib/column_widget.dart
+++ lib/column_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColumnWidget extends StatelessWidget { 
-  const ColumnWidget({Key? key}): super(key: key);
+  const ColumnWidget({super.key});
 
 @override 
 Widget build(BuildContext context) { 
